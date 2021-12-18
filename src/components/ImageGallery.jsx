35cf7ae28b0a47326e6b1b0a53ef6b21947533ed
Joanna-Golofit <sunderlajsa@gmail.com>
2021-12-18 13:31:17 +0100

    przy load more laduja sie kolejne obrazy zamiast dodawac do istniejacych

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { getApiData } from "../utils/apiCalls";
-// import { IMAGE_URL } from "../utils/data";
+import { getApiData } from "./../utils/apiCalls";
+// import { IMAGE_URL } from "./../utils/data";
 import ImageGalleryItem from "./ImageGalleryItem";
 import Button from "./Button";
 
@@ -17,24 +17,19 @@ const ImageGallery = () => {
     getApiData(
       `https://pixabay.com/api/?key=23726584-b0725e8cc2245e4091c11b21f&q=${search}&page=${page}&image_type=photo&orientation=horizontal&per_page=4`
     )
-      .then((data) => {
-        console.log("pobrano z Api:", data.hits);
-        setFetchedImages(data.hits);
-      })
-      .catch((err) => {
-        console.log("moj log z error.name", err.name);
-        setError(err);
-      })
+      .then((data) => setFetchedImages(data.hits))
+      .catch((err) => setError("moj log z error.name", err.name))
       .finally(() => setIsLoading(false));
   }, [page, search]);
 
   return (
     <>
-      <Button onClick={() => {
-        setPage(page + 1);
-        console.log(page)
-      }
-      } />
+      <Button
+        onClick={() => {
+          setPage(page + 1);
+          console.log(page);
+        }}
+      />
 
       {error && <p>Whoops, something went wrong: {error.message}</p>}
       {isLoading && <p>Loading Api data...</p>}
@@ -51,29 +46,27 @@ const ImageGallery = () => {
 
 export default ImageGallery;
 
-
-
 // key: 23726584 - b0725e8cc2245e4091c11b21f;
 // https://pixabay.com/api/
 
 // https://pixabay.com/api/?key=23726584-b0725e8cc2245e4091c11b21f&q=yellow+flowers&image_type=photo
 
-  // useEffect(() => {
-  //   if (getStorageItems().length === 0) {
-  //     setIsLoading(true);
-  //     getApiData(IMAGE_URL)
-  //       .then((data) => {
-  //         console.log("pobrano z Api:", data.hits);
-  //         setStorageItems(data.hits);
-  //         setFetchedImages(data.hits);
-  //       })
-  //       .catch((err) => {
-  //         console.log("moj log z error.name", err.name);
-  //         setError(err);
-  //       })
-  //       .finally(() => setIsLoading(false));
-  //   } else {
-  //     setFetchedImages(getStorageItems());
-  //     console.log("localStorage:", JSON.parse(localStorage.getItem("picts")));
-  //   }
-  // }, []);
+// useEffect(() => {
+//   if (getStorageItems().length === 0) {
+//     setIsLoading(true);
+//     getApiData(IMAGE_URL)
+//       .then((data) => {
+//         console.log("pobrano z Api:", data.hits);
+//         setStorageItems(data.hits);
+//         setFetchedImages(data.hits);
+//       })
+//       .catch((err) => {
+//         console.log("moj log z error.name", err.name);
+//         setError(err);
+//       })
+//       .finally(() => setIsLoading(false));
+//   } else {
+//     setFetchedImages(getStorageItems());
+//     console.log("localStorage:", JSON.parse(localStorage.getItem("picts")));
+//   }
+// }, []);
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,6 +1,8 @@
     // const IMAGE_URL = `https://pixabay.com/api/?key=23726584-b0725e8cc2245e4091c11b21f&q=${search}&page=${page}&image_type=photo&orientation=horizontal&per_page=6`;
+
     // const IMAGE_URL =
-    //   "https://pixabay.com/api/?key=23726584-b0725e8cc2245e4091c11b21f&q=cat&page=1&image_type=photo&orientation=horizontal&per_page=6";
+      "https://pixabay.com/api/?key=23726584-b0725e8cc2245e4091c11b21f&q=cat&page=1&image_type=photo&orientation=horizontal&per_page=6";
+
     // const IMAGE_URL = "https://pixabay.com/api/?key=23726584-b0725e8cc2245e4091c11b21f&q=yellow+flowers&image_type=photo";
 
     // export { IMAGE_URL };
