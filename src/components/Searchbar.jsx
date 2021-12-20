f1556015471d6e5fc3eff16217073c7e701df5e3
Joanna-Golofit <sunderlajsa@gmail.com>
2021-12-20 11:13:54 +0100

    nieudana proba z useForm.js

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,29 +1,30 @@
+// import { useForm } from "./../utils/";
 
 const SearchBar = () => {
+
+  // const [values, handleChange] = useForm({ input: ""});
+ 
   return (
     <header
-      className="Searchbar {
-"
-    >
+      className="Searchbar"    >
       <form className="SearchForm">
         <button type="submit" className="SearchForm-button">
           <span className="SearchForm-button-label">Search</span>
         </button>
 
         <input
+          // value={values.input}
+          name="input"
           className="SearchForm-input"
           type="text"
           autoComplete="off"
           autoFocus
           placeholder="Search images and photos"
+          // onChange={handleChange}
         />
       </form>
     </header>
   );
-}
+};
 
-export default SearchBar
-
-
-
-
+export default SearchBar;
