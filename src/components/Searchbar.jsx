82de6d46aca4dabf4a179376ed7db1130b02e396
Joanna-Golofit <sunderlajsa@gmail.com>
2021-12-20 14:46:05 +0100

    Searchbar - zlapane value z inputa

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,26 +1,33 @@
-// import { useForm } from "./../utils/";
+import { useState } from "react";
 
 const SearchBar = () => {
 
-  // const [values, handleChange] = useForm({ input: ""});
- 
+  const [search, setSearch] = useState({value: ""});
+   
   return (
-    <header
-      className="Searchbar"    >
+    <header className="Searchbar">
       <form className="SearchForm">
         <button type="submit" className="SearchForm-button">
           <span className="SearchForm-button-label">Search</span>
         </button>
 
         <input
-          // value={values.input}
           name="input"
+          value={search.value}
           className="SearchForm-input"
           type="text"
           autoComplete="off"
           autoFocus
           placeholder="Search images and photos"
           // onChange={handleChange}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setSearch({
+              ...search,
+              value : e.target.value,
+            });
+            console.log(search);
+          }}
         />
       </form>
     </header>
