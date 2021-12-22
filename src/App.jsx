c538e8303e59d751204db95c1b59121ed69af792
Joanna-Golofit <sunderlajsa@gmail.com>
2021-12-22 20:12:28 +0100

    spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // import './App.css';
 import { useState, useEffect } from "react";
 import { getApiData } from "./utils/apiCalls";
-
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 import "./components/styles.css";
 // import ImageGalleryItem from "./components/ImageGalleryItem";
 import ImageGallery from "./components/ImageGallery";
@@ -13,10 +14,12 @@ import Button from "./components/Button";
 function App() {
   const [fetchedImages, setFetchedImages] = useState([]);
   const [fetchedImagesTotal, setFetchedImagesTotal] = useState(0);
-  const [page, setPage] = useState(12);
+  const [perPage, setPerPage] = useState(12);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [query, setQuery] = useState("");
+  // const [query, setQuery] = useState("null");
+  // const [query, setQuery] = useState("nul");
+  const [query, setQuery] = useState("nut");
 
 
   useEffect(() => {
@@ -24,23 +27,22 @@ function App() {
     // getApiData(IMAGE_URL)
     getApiData(
       // `https://pixabay.com/api/?key=23726584-b0725e8cc2245e4091c11b21f&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=6`
-      `https://pixabay.com/api/?key=23726584-b0725e8cc2245e4091c11b21f&q=${query}&page=1&image_type=photo&orientation=horizontal&per_page=${page}`
+      `https://pixabay.com/api/?key=23726584-b0725e8cc2245e4091c11b21f&q=${query}&page=1&image_type=photo&orientation=horizontal&per_page=${perPage}`
     )
       .then((data) => {
         setFetchedImages(data.hits);
-      setFetchedImagesTotal(data.total);
+        setFetchedImagesTotal(data.total);
       })
       .catch((err) => setError("moj log z error.name", err.name))
       .finally(() => {
         setIsLoading(false);
       });
-  }, [page, query]);
+  }, [perPage, query]);
 
   const getKeyword = (inputValueFromSearchBar) => {
     //przesylana w propsach do searchbar i tam otrzymuje wartosc z inputa i ustawia ja tu jako query i przy okazji ustawia strone na 1
     setQuery(inputValueFromSearchBar);
-      setPage(12);
-
+      setPerPage(12);
   };
 
   return (
@@ -53,16 +55,29 @@ function App() {
           <ImageGalleryItem
             fetchedImages={fetchedImages}
             // onClick={this.handleClick}
-            />
+          />
         </ImageGallery>
       )}
-      {isLoading && <p>Loader..?</p>}
-      {fetchedImages.length} z {fetchedImagesTotal}
+      {/* {isLoading && <p>Loader..?</p>} */}
+      {isLoading && (
+        <Loader
+          style={{ textAlign: "center" }}
+          type="MutatingDots"
+          color="#3f51b5"
+          secondaryColor="lightgray"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+        />
+      )}
+      <p style={{ textAlign: "center" }}>
+        {fetchedImages.length} z {fetchedImagesTotal}
+      </p>
       {!isLoading && fetchedImagesTotal > fetchedImages.length && (
         <Button
           onClick={() => {
-            setPage((page) => page + 12);
-            console.log(page, query);
+            setPerPage((perPage) => perPage + 12);
+            console.log(perPage, query);
           }}
         />
       )}
